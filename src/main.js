6313458e7c65ee1880e181abadd6fdba2015562a
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2021-04-10 00:23:22 -0700

    debugging

--- src/main.js
+++ src/main.js
@@ -7,14 +7,11 @@ import 'bootstrap/dist/css/bootstrap.css' //Bootstrap Vue
 import 'bootstrap-vue/dist/bootstrap-vue.css' //Bootstrap Vue
 import { IconsPlugin } from 'bootstrap-vue' //Bootstrap Vue
 
-import VuePlyr from '/node_overwrites/vue-plyr-ssr.js' // Vue Plyr
-import 'vue-plyr/dist/vue-plyr.css' // Vue Plyr
 import VideoBg from 'vue-videobg' //Video Background Player
 
 import Embed from '/node_overwrites/v-video-embed-ssr.js' // Video Embedding
 
 import VueAxios from 'vue-axios' // Vue Axios
-import VueAuthenticate from 'vue-authenticate' // Vue-Authenticate (Github OAuth)
 
 import axios from 'axios';
 
@@ -26,23 +23,7 @@ export default function (Vue, { router, head, isClient }) {
   Vue.component('Layout', DefaultLayout)
   Vue.use(BootstrapVue) // Bootstrap Vue
   Vue.use(IconsPlugin) // Bootstrap Vue 
-  Vue.use(VuePlyr, {
-    plyr: {}
-  })
   Vue.use(Embed)
-  Vue.component('video-bg', VideoBg) //Video Background Player
-  
+  Vue.component('video-bg', VideoBg) //Video Background Player  
   Vue.use(VueAxios, axios) // Vue Axios
-  Vue.use(VueAuthenticate, { // Vue Authenticate
-    baseUrl: domain, // Your API domain
-
-    providers: {
-      github: {
-        clientId: clientId,
-        redirectUri: '/auth/github/callback'// Your client app URL
-      }
-    }
-  })
-
-
 }
