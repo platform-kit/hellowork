57982b0aa4cfa30fcb302ef4eca61118f0f8077b
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2021-04-11 03:30:46 -0700

    replaced gitorm with octokit

--- functions/content-read-v1/content-read-v1.js
+++ functions/content-read-v1/content-read-v1.js
@@ -1,6 +1,6 @@
-var gitorm = require('gitorm');
 var axios = require('axios');
 require('dotenv').config();
+const { Octokit } = require("@octokit/rest");
 var base64 = require('js-base64').Base64;
 
 var token = process.env.GITHUB_TOKEN;
@@ -9,102 +9,118 @@ var repo = process.env.GITHUB_REPO;
 
 
 
+
+
+
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
-  
-  var params = event.queryStringParameters || null;
 
+  // Params
+  var params = event.queryStringParameters || null;
   if (typeof params.data == 'string') {
     params.data = JSON.parse(params.data);
   }
-
+  const fileName = params.file || 'static/data.json'
   console.log(params);
 
-  // Initialize Gitorm
-  const Gitorm = new gitorm({
-    token: token,
-    repository: repo,
-    owner: user
+  //
+  const octokit = new Octokit({
+    auth: token,
   });
-
-  // Initialize Gitorm
-  const fileName = params.file || 'static/data.json'
-  await Gitorm.connect();
-  console.log(Gitorm.status);
+  var sha = null;
+  var output = await octokit.rest.repos
+    .getContent({
+      owner: user,
+      repo: repo,
+      path: fileName,
+    })
+    .then(({ data }) => {
+      sha = data.sha;
+      data = base64.decode(data.content);
+      // handle data
+      //console.log(data);
+     return data;
+    });
 
   // Check password
   var password = false;
-  if(params.p == process.env.PASSWORD) {
+  if (params.p == process.env.PASSWORD) {
     password = true;
   }
-  
-  // Update the file
-  if(event.body != null && password == true){ 
-    const updatedFile = await Gitorm.update({
-      data: JSON.stringify(JSON.parse(event.body)),
-      path: `${fileName}`
-    });
-  }
 
-  // Read the file
-  const file = await Gitorm.find({
-    path: `${fileName}`
-  });  
-  console.log(file);
-
-  var content = '';
-
-  // Make a request for a user with a given ID
-  await axios.get(file.url)
-    .then(function (response) {
-      // handle success
-      console.log(response);
-      content = JSON.parse(base64.decode(response.data.content));
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-
-    });
-
-  file.content = content;
+  var error = null;
   var status = 200;
-  var message = "Success.";  
-  if(event.body != null && password == false){
-    status = 500;
-    message = "Incorrect Password.";
+
+  // Update the file, if new data is provided & password is correct
+  
+  if (event.body != null && password == true) {
+    
+    try {
+    var file = await octokit.rest.repos
+      .createOrUpdateFileContents({
+        owner: user,
+        repo: repo,        
+        path: fileName,        
+        branch: 'main',
+        sha: sha,
+        message: "Content Updated",
+        content: base64.atob(JSON.stringify(JSON.parse(event.body))),
+      })
+      .then(({ data }) => {
+        data = JSON.parse(base64.decode(data.content));
+        // handle data
+        //console.log(data);
+      });
+    }
+    catch(err) {
+      error = err;
+      status = 500;
+    }          
+
+    
+    var message = "Success.";
+    if (event.body != null && password == false) {
+      status = 500;
+      message = "Incorrect Password.";
+    }
+
+    var body = null;
+    if (event.body != null && event.body != '') {
+      console.log(' ---- ');
+      console.log(typeof event.body);
+      console.log(' ---- ');
+      body = JSON.parse(event.body);
+    }
+
+    var data = {
+      "message": message,
+      params: params,
+      body: body,
+      file: file,
+    };
+
+    var output = null;
+
+    if (event.body == null || params.p == null) {
+      output = content;
+    }
+
+    else {
+      output = data;
+    }
+    
+    
+
+    }
+
+    if(typeof output == 'string') {
+      output = JSON.parse(output);
+    }
+
+    status = 200;
+    return {
+      statusCode: status,
+      error: error,
+      body: JSON.stringify(output)
+    };
   }
-
-  var body = null;
-  if(event.body != null && event.body != ''){
-    console.log(' ---- ');
-    console.log(typeof event.body);
-    console.log(' ---- ');
-    body = JSON.parse(event.body);
-  }  
-
-  var data = {
-    "message": message,
-    params: params,
-    body: body,
-    file: file,
-  };
-
-  var output = null;
-
-  if(event.body == null || params.p == null){
-    output = content;
-  }
-
-  else {
-    output = data;
-  }
-
-  return {
-    statusCode: status,
-    body: JSON.stringify(output)
-  };
-}
