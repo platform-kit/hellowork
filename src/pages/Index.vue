4554bf616e03b4a1afda8a5a856f5cac14c14123
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2021-04-13 00:55:23 -0700

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -887,7 +887,7 @@ export default {
         generatedPostImage: null,
         text: null,
         image:
-          "https://images.unsplash.com/photo-1618178498578-68352fccdc6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1234&q=80",
+          "https://source.unsplash.com/daily",
         overlay: "/images/overlays/overlay.png",
         config: {
           inline: {
